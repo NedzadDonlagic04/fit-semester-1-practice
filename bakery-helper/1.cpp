023df5e2c43a6144bf631a73a6fa74db3ed18cf4
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-12 12:51:48 +0200

    Added problem description, possible solution and flowchart

---
+++ bakery-helper/1.cpp
@@ -0,0 +1,74 @@
+// Done by YourFBIAgent
+#include<iostream>
+#include<string>
+#include<array>
+#include<iomanip>
+#include<limits>
+
+bool isInputAValidFloat(const std::string &input) {
+    auto dotLimitReached { false };
+
+    for(const auto &c : input) {
+        if(!dotLimitReached && c == '.') dotLimitReached = true;
+        else if(c < 48 || c > 57 || (c == '.' && dotLimitReached)) return false;
+    }
+    return true;
+}
+
+void clearInputBuffer() {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+/*
+    Performs user input by converting the input to a string
+    and then trying to convert it to a float, if it fails this
+    means the entered value isn't a number and the user needs
+    to redo the input properly
+*/
+void handleInput(float &var, const std::string &inputText) {
+    std::string temp {};
+    auto inputValid {true};
+
+    while(1) {
+        std::cout<<inputText<<": ";
+        std::cin>>temp;
+        
+        if(isInputAValidFloat(temp)) {
+            var = std::stof(temp);
+            break;
+        } else {
+            std::cout<<"\nError! Input has to be a valid decimal number!\n"; 
+            clearInputBuffer();
+        }
+    }
+
+    clearInputBuffer();
+}
+
+int main(int argv, char* argc[]) {
+    std::array<std::string, 7> daysOfTheWeek = { 
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday", 
+        "Sunday" 
+    };
+
+    float currentAmountOfMoney {}, averageEarnedPerDay {};
+
+    handleInput(currentAmountOfMoney, "Enter the amount of money at the start of the week");
+    handleInput(averageEarnedPerDay, "Enter the average amount of money earned per day");
+
+    std::cout<<'\n';
+    std::cout<<std::fixed<<std::setprecision(2);
+    for(const auto &day : daysOfTheWeek) {
+        currentAmountOfMoney += averageEarnedPerDay;
+        
+        std::cout<<"At the end of "<<day<<"\'s workday you will have earned "<<currentAmountOfMoney<<"$\n";
+    }
+
+    return 0;
+}
---
+++ bakery-helper/README.md
@@ -0,0 +1,24 @@
+# bakery-helper
+
+Fahra runs an old bakery down the street you live in.
+One time you went there she mentioned hearing about
+your achievements in the world of programming and has
+asked you to make her a program to calculate and display
+the amount of money she'd accumulate for a given week.
+(Yes the bakery works every day).
+
+Fahra would give you a starting amount of money and the 
+average she'd receive at the end of each day.
+
+Your task is to create a program that achieves this in whatever
+way you see fit, you may use anything from the standard library,
+your only restrictions would be making sure the numbers entered
+would never be negative or 0 (so natural numbers only) and that
+they never exceed a certain amount of your choosing.
+
+I.E. the amount of starting money probably isn't 100 000 
+and the average per day isn't 10 000 considering it's an old 
+bakery down the street.
+
+With this problem an example flowchart for a possible solution
+does exist.
