d7d3aa6dc354d5852518646fdb9869abce412cc5
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-15 13:36:39 +0200

    Updated script so it'd update the README with new problem name as well

--- generator.py
+++ generator.py
@@ -1,7 +1,12 @@
 import sys
 import os
 
-problem_path = f'problems/{sys.argv[1]}'
+"""
+    CREATE PROBLEM DIRECTORY
+"""
+
+problem_name = sys.argv[1]
+problem_path = f'problems/{problem_name}'
 
 os.mkdir(f'{problem_path}')
 os.mkdir(f'{problem_path}/img')
@@ -18,3 +23,25 @@ int main(int argv, char* argc[]) {
     return 0;
 }
 """)
+
+
+"""
+    UPDATE README CODE 
+"""
+
+# This part for getting the last line of the file
+# will need to be refactored in the future due to
+# this not being very optimal for big file sizes
+last_line = None
+with open('README.md') as f:
+    for line in f:
+        pass
+    last_line = line
+
+# take the last line and split it based on the . and the first
+# member of the given list will be a number
+# increment it by one so it continues the order
+problem_number = int(last_line.split('.')[0]) + 1
+
+with open('README.md', 'a') as f:
+    f.write(f'\n{problem_number}. {problem_name}')
