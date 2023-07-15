7e71c9a2e3b256dabb074a2c1e7524062a5fddcc
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-15 12:17:51 +0200

    Updated generator script

--- generator.py
+++ generator.py
@@ -1,15 +1,15 @@
 import sys
 import os
 
-problem_folder_name = sys.argv[1]
+problem_path = f'problems/{sys.argv[1]}'
 
-os.mkdir(f'{problem_folder_name}')
-os.mkdir(f'{problem_folder_name}/img')
+os.mkdir(f'{problem_path}')
+os.mkdir(f'{problem_path}/img')
 
-with open(f'{problem_folder_name}/README.md', 'w') as readme:
-    readme.writelines([f"# {problem_folder_name}\n\n", 'Description text goes here'])
+with open(f'{problem_path}/README.md', 'w') as readme:
+    readme.writelines([f"# {problem_path}\n\n", 'Description text goes here'])
 
-with open(f'{problem_folder_name}/1.cpp', 'w') as task_file:
+with open(f'{problem_path}/1.cpp', 'w') as task_file:
     task_file.write("""#include<iostream>
 
 int main(int argv, char* argc[]) {
