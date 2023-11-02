953231d3baad4a40091979023169872121239540
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-11-02 10:08:58 +0100

    Added declaration and definition of merge sort with recusion

--- problems/merge-sort/includes/mergeSort.hpp
+++ problems/merge-sort/includes/mergeSort.hpp
@@ -5,7 +5,7 @@
 #ifndef MERGE_SORT_H
 #define MERGE_SORT_H
 
-void recursiveMergeSort(const std::vector<int> &arr, bool sortAscending=true);
-void nonRecursiveMergeSort(const std::vector<int> &arr, bool sortAscending=true);
+[[nodiscard]] std::vector<int> recursiveMergeSort(const std::vector<int> &arr, bool sortAscending=true);
+[[nodiscard]] std::vector<int> nonRecursiveMergeSort(const std::vector<int> &arr, bool sortAscending=true);
 
 #endif 
--- problems/merge-sort/src/mergeSort.cpp
+++ problems/merge-sort/src/mergeSort.cpp
@@ -1,9 +1,46 @@
 #include "./../includes/mergeSort.hpp"
+#include <cstddef>
+#include <iostream>
 
-void recursiveMergeSort(const std::vector<int> &arr, bool isAscending) {
+[[nodiscard]] std::vector<int> merge(std::vector<int> &leftVec, std::vector<int> &rightVec) {
+  std::size_t leftIndex { 0 }, rightIndex { 0 };
 
+  std::vector<int> returnVec (leftVec.size() + rightVec.size());
+
+  while (leftIndex != leftVec.size() && rightIndex != rightVec.size()) {
+    if (leftVec[leftIndex] < rightVec[rightIndex]) {
+      returnVec[leftIndex + rightIndex] = leftVec[leftIndex];
+      leftIndex++;
+    } else {
+      returnVec[leftIndex + rightIndex] = rightVec[rightIndex];
+      rightIndex++;
+    }
+  }
+
+  while (leftIndex != leftVec.size()) {
+      returnVec[leftIndex + rightIndex] = leftVec[leftIndex];
+      leftIndex++;
+  }
+
+  while (rightIndex != rightVec.size()) {
+      returnVec[leftIndex + rightIndex] = rightVec[rightIndex];
+      rightIndex++;
+  }
+  
+  return returnVec;
 }
 
-void nonRecursiveMergeSort(const std::vector<int> &arr, bool isAscending) {
+[[nodiscard]] std::vector<int> recursiveMergeSort(const std::vector<int> &vec, bool isAscending) {
+  if (vec.size() == 1) return vec;
 
+  auto size { vec.size() };
+
+  auto leftHalfVec { recursiveMergeSort(std::vector(&vec[0], &vec[size / 2])) };
+  auto rightHalfVec { recursiveMergeSort(std::vector(&vec[size / 2], &vec[size])) };
+
+  return merge(leftHalfVec, rightHalfVec); 
+}
+
+[[nodiscard]] std::vector<int> nonRecursiveMergeSort(const std::vector<int> &vec, bool isAscending) {
+  return vec;
 }
