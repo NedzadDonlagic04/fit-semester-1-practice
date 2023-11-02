2e74001f205e6aea1af4af3b0ee6feb030d67ddd
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-11-02 10:09:32 +0100

    Added test for merge sort
    It works :)

--- problems/merge-sort/1.cpp
+++ problems/merge-sort/1.cpp
@@ -2,7 +2,9 @@
 #include "./includes/mergeSort.hpp"
 
 int main() {
-    nonRecursiveMergeSort({1, 2, 3, 4});
+    auto idk {recursiveMergeSort({9, 1, 5, 4, 2, 0}) };
+
+    for (const auto &i : idk) std::cout<<i<<'\n';
 
     return 0;
 }
