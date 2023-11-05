d6bbdcf29323cb1cd7725e37232ad5fa6d24af7e
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-11-06 00:51:19 +0100

    Changed function name

--- problems/merge-sort/includes/mergeSort.hpp
+++ problems/merge-sort/includes/mergeSort.hpp
@@ -6,6 +6,6 @@
 #define MERGE_SORT_H
 
 [[nodiscard]] std::vector<int> recursiveMergeSort(const std::vector<int> &arr, bool sortAscending=true);
-[[nodiscard]] std::vector<int> nonRecursiveMergeSort(const std::vector<int> &arr, bool sortAscending=true);
+void iterativeMergeSort(std::vector<int> &arr, bool sortAscending=true);
 
 #endif 
