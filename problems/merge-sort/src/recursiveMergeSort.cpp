586501fd3723ff000345fcfd41b3e0ddf5d68380
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-11-06 00:51:45 +0100

    Moved iterative merge sort into different file
    Moved increment into [] operator for what I consider cleaner
    code

--- problems/merge-sort/src/recursiveMergeSort.cpp
+++ problems/merge-sort/src/recursiveMergeSort.cpp
@@ -8,13 +8,10 @@
   std::vector<int> returnVec (leftVec.size() + rightVec.size());
 
   while (leftIndex != leftVec.size() && rightIndex != rightVec.size()) {
-    if (leftVec[leftIndex] < rightVec[rightIndex]) {
-      returnVec[leftIndex + rightIndex] = leftVec[leftIndex];
-      leftIndex++;
-    } else {
-      returnVec[leftIndex + rightIndex] = rightVec[rightIndex];
-      rightIndex++;
-    }
+    if (leftVec[leftIndex] < rightVec[rightIndex]) 
+      returnVec[leftIndex + rightIndex] = leftVec[leftIndex++];
+    else 
+      returnVec[leftIndex + rightIndex] = rightVec[rightIndex++];
   }
 
   while (leftIndex != leftVec.size()) {
@@ -39,15 +36,4 @@
   auto rightHalfVec { recursiveMergeSort(std::vector(&vec[size / 2], &vec[size])) };
 
   return merge(leftHalfVec, rightHalfVec); 
-}
-
-[[nodiscard]] std::vector<int> nonRecursiveMergeSort(const std::vector<int> &vec, bool isAscending) {
-  for (std::size_t i = 2; i < vec.size(); i *= 2) {
-    for (std::size_t ii = 0; ii < vec.size(); ii += i) {
-      std::size_t middleVec { ii + i }, endVec { std::min(ii + i * 2 - 1, vec.size()) };
-
-    }
-  }
-
-  return vec;
 }
